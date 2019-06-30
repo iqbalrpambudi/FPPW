314da373a163713a7bcccabaf328d207fe32f5fa
Iqbal <iqbalrpambudi@gmail.com>
2019-06-30 11:29:12 +0700

    Update terbaru

--- js/js.js
+++ js/js.js
@@ -2,9 +2,11 @@ window.onscroll = function()
 {
     myFunction()
 };
-
+// Sticky Navbar
 var navbar = document.getElementById("menu");
 var sticky = navbar.offsetTop;
+var nama;
+var telp;
 
 function myFunction() 
 {
@@ -16,4 +18,21 @@ function myFunction()
     {
         navbar.classList.remove("sticky");
     }
-} 
+};
+
+
+// function GetURLParameter(sParam)
+// {
+//     var sPageURL = window.location.search.substring(1);
+//     var sURLVariables = sPageURL.split('&');
+//     for (var i = 0; i < sURLVariables.length; i++)
+//     {
+//         var sParameterName = sURLVariables[i].split('=');
+//         if (sParameterName[0] == sParam)
+//         {
+//             return sParameterName[1];
+//         }
+//     }
+// }​
+
+
